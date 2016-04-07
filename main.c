3491d3c36f87a73d25e624be7e4114dc2eda9240
Paweł Soja <Pawe Soja>
2016-04-08 00:26:43 +0200

    Committer: Paweł Soja <Paweł Soja>  -czujniki  -diody reaguje na czujniki  -init  -podstawowe rzeczy

--- main.c
+++ main.c
@@ -1,16 +1,38 @@
-#include "stm32f4xx_conf.h"
-#include "stm32f4xx_gpio.h"
+/*
+To do:(wtorek)
+-oddzielnie biblioteki na inity
+-obsluga czujników z timerami
+-glosnik
+-diody na drugiej plytce
+-stabilizacja
+ */
+
+#include "stm32f4xx.h"
 #include "stm32f4xx_rcc.h"
+#include "stm32f4xx_gpio.h"
+#include "stm32f4xx_dac.h"
+
+/*opoznienie*/
+void delay(int a)
+{
+int i=0;
+for(i;i<a;i++)
+{}
+}
 
 int main(void)
 {
 	SystemInit();
-
-	/* GPIOD Periph clock enable */
+	/*RCC*/
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOE, ENABLE);
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_DAC, ENABLE); //DAC
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE); //ADC
 
-	GPIO_InitTypeDef  GPIO_InitStructure;
-	/* Configure PD12, PD13, PD14 and PD15 in output pushpull mode */
+	/*GPIO diody*/
+	GPIO_InitTypeDef GPIO_InitStructure;
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13| GPIO_Pin_14| GPIO_Pin_15;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
 	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
@@ -18,14 +40,44 @@ int main(void)
 	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
 	GPIO_Init(GPIOD, &GPIO_InitStructure);
 
-	unsigned int i;
+	/*GPIO czujniki*/
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4 | GPIO_Pin_2;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
 
-	for(;;)
-	{
-		GPIO_SetBits(GPIOD, GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15);
-		for (i=0;i<1000000;i++);
-		GPIO_ResetBits(GPIOD, GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15);
-		for (i=0;i<1000000;i++);
-	}
+	/*DAC g³osnik+diody na drugiej plytce*/
+	DAC_InitTypeDef DAC_InitStructure;
+	DAC_InitStructure.DAC_Trigger = DAC_Trigger_None;
+	DAC_InitStructure.DAC_WaveGeneration = DAC_WaveGeneration_None;
+	DAC_InitStructure.DAC_LFSRUnmask_TriangleAmplitude = DAC_LFSRUnmask_Bit0;
+	DAC_InitStructure.DAC_OutputBuffer = DAC_OutputBuffer_Enable;
+	DAC_Init(DAC_Channel_1, &DAC_InitStructure);
+	DAC_Cmd(DAC_Channel_1, ENABLE);
 
+    while(1)
+    {
+    	if(GPIO_ReadInputDataBit(GPIOC, GPIO_Pin_7)==1)//jezeli bit na PC7=1 -> pomarañczowa dioda
+    	{
+    		GPIO_SetBits(GPIOD, GPIO_Pin_15);
+    		GPIO_ResetBits(GPIOD, GPIO_Pin_15);
+    	}
+    	else
+    	{
+    		GPIO_ResetBits(GPIOD, GPIO_Pin_15);
+    	}
+
+    	if(GPIO_ReadInputDataBit(GPIOC, GPIO_Pin_6)==1) //jezeli bit na PC7=1 -> niebieska dioda
+    	{
+    	    GPIO_SetBits(GPIOD, GPIO_Pin_13);
+    	    GPIO_ResetBits(GPIOD, GPIO_Pin_13);
+    	}
+    	else
+    	{
+    		GPIO_ResetBits(GPIOD, GPIO_Pin_13);
+    	}
+    }
 }
+
+
